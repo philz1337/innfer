c4c8bf9e0750334c8e361e80fe2dbc6959674c6c
p <pberben@gmx.de>
2023-07-21 09:06:23 +0200

    x

--- predict.py
+++ predict.py
@@ -4,8 +4,14 @@ import torch
 class Predictor(BasePredictor):
     def setup(self):
         self.model = torch.load("./models//4x_Faces_04_N_180000_G.pth")
+        
 
     def predict(self,
-          image: Path = Input(description="input image")
+          image: Path = Input(description="input image"),
+          version: str = Input(
+                description='GFPGAN version',
+                choices=['8x_NMKD-Faces_160000_G'],
+                default='8x_NMKD-Faces_160000_G'),
+        scale: float = Input(description='Rescaling factor', default=2),
     ) -> Path:
         return (image)
