475298fbe6a935098fa22d3fc0c695c6a7f7c9b2
p <pberben@gmx.de>
2023-07-21 10:31:42 +0200

    x

--- predict.py
+++ predict.py
@@ -23,19 +23,11 @@ class Predictor(BasePredictor):
 
         # Execute the command in the background
         command = f"python run.py -m {model_path} -o {output_path} -scale {scale}"
-        process = subprocess.Popen(command, shell=True)
+        subprocess.run(command, shell=True)
 
-        # Wait for the process to finish and the file to be created
-        max_wait_time = 60  # Maximum 60 seconds of waiting
-        start_time = time.time()
-        while process.poll() is None:
-            if time.time() - start_time > max_wait_time:
-                process.terminate()  # Terminate the process if it exceeds the time limit
-                raise TimeoutError("The process did not finish on time.")
-            time.sleep(1)
-
-        # Check if the file exists
         if os.path.exists(output_path):
             return Path(output_path)
+        elif os.path.exists("/output/raw.png"):
+            return Path("/output/raw.png")
         else:
             raise FileNotFoundError("The file could not be created.")
