e99f6aba46cfd8e9455c7c2ccfceb767324bce0a
p <pberben@gmx.de>
2023-07-21 09:51:49 +0200

    x

--- predict.py
+++ predict.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import time
 from cog import BasePredictor, Input, Path
 import torch
 
@@ -20,13 +21,21 @@ class Predictor(BasePredictor):
         output_path = "/tmp/seed-1.png"
         model_path = f"./models/{version}.pth"
 
+        # Execute the command in the background
         command = f"python run.py -m {model_path} -o {output_path} -scale {scale}"
         process = subprocess.Popen(command, shell=True)
 
+        # Wait for the process to finish and the file to be created
+        max_wait_time = 60  # Maximum 60 seconds of waiting
+        start_time = time.time()
         while process.poll() is None:
-            continue
+            if time.time() - start_time > max_wait_time:
+                process.terminate()  # Terminate the process if it exceeds the time limit
+                raise TimeoutError("The process did not finish on time.")
+            time.sleep(1)
 
+        # Check if the file exists
         if os.path.exists(output_path):
             return Path(output_path)
         else:
-            raise FileNotFoundError("No file found.")
+            raise FileNotFoundError("The file could not be created.")
